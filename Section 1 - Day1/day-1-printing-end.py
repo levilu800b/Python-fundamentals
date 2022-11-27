c6e2fc3a8f7df1acef341a3dc47d3a5a560584a3
levilu800b <levilu800b@gmail.com>
2022-11-27 19:55:43 +0000

    First python commit

---
+++ Section 1 - Day1/day-1-printing-end.py
@@ -0,0 +1,15 @@
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name?"))
+
+# Write your code below this line 👇
+# print function
+# print("Hello World!")
+# print("Hello World!\nHello World!")
+# print ("Hello " + "Levi")
+# print("hello" + " " + "Levi" )
+
+# input function
+# input("What is your name?")
+# Then print() will print the word "Hello" and the users input
+print("Hello " + input("What is your name?") + "!")
