7e668a6f61450ea4c1232226b1120534caa6825b
levilu800b <levilu800b@gmail.com>
2022-11-27 20:25:05 +0000

    day 1

---
+++ Section 1 - Day1/day-1-printing-start.py
@@ -0,0 +1,11 @@
+# Write your code below this line 👇
+# print function
+# print("Hello World!")
+# print("Hello World!\nHello World!")
+# print ("Hello " + "Levi")
+# print("hello" + " " + "Levi" )
+
+# input function
+# input("What is your name?")
+# Then print() will print the word "Hello" and the users input
+print("Hello " + input("What is your name?") + "!")
