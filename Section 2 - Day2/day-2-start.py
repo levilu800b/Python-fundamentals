31035f5d4f9d644678d67c0aa07835ef16a1fcff
levilu800b <levilu800b@gmail.com>
2022-11-29 18:38:54 +0000

    data type start

---
+++ Section 2 - Day2/day-2-start.py
@@ -0,0 +1,60 @@
+# Data Types
+
+# string
+
+# print("Hello"[4])
+
+# integer
+
+# print(123 + 345)
+
+# 123_456_789
+
+# float
+
+# 3.14159
+
+# boolean
+
+# True
+# False
+
+# num_char = len(input("What is your name?"))
+# print("Your name has " + num_char + " characters.") # will give an error
+
+# num_char = len(input("What is your name?"))
+# print(type(num_char))
+
+# num_char = len(input("What is your name?"))
+
+# new_num_char = str(num_char)
+
+# print("Your name has " + new_num_char + " characters.")
+
+# a = 123
+# print(type(a))
+
+# a = str(123)
+# print(type(a))
+
+# a = float(123)
+# print(type(a))
+# print(70 + float("100.5"))
+# print(str(70) + str(100))
+
+# Math operation
+
+# 3 + 5
+# 7 - 4
+# 3 * 2
+# 6 / 3
+# 2 ** 2
+# ** - exponent
+
+# PEMDAS
+# Parentheses - ()
+# Exponents - **
+# Multiplication - *
+# Division - /
+# Addition - +
+# Subtraction - -
