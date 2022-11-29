d9d4226a872d14aa86c565e1d654954b0ad1b01b
levilu800b <levilu800b@gmail.com>
2022-11-29 09:36:29 +0000

    error check

--- Section 1 - Day1/day-1-variables-start.py
+++ Section 1 - Day1/day-1-variables-start.py
@@ -10,6 +10,6 @@ name = "Peters"
 print(name)
 
 # print(len ( input("What is your name?")))
-name = input ("What is your name? ")
+name = input("What is your name? ")
 length = len(name)
 print(length)
