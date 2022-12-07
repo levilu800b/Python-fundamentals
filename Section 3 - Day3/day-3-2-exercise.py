72e8bdf5db12042f8fc3abcd0f28babe405cb82a
levilu800b <levilu800b@gmail.com>
2022-12-07 09:11:46 +0000

    Day 3 exercise2

--- Section 3 - Day3/day-3-2-exercise.py
+++ Section 3 - Day3/day-3-2-exercise.py
@@ -41,3 +41,25 @@
 # Try to use the exponent operator in your code. Remember to round your result to the nearest whole number. Make sure
 # you include the words in bold from the interpretations. Test Your Code Check your code is doing what it is supposed
 # to. When you're happy with your code, click submit to check your solution.
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+bmi = round(weight / (height ** 2))
+
+if bmi <= 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi <= 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi <= 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi <= 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+
+
