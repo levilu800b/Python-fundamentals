affd8ef0a63aca0245cb6fabbc800d418579d458
levilu800b <levilu800b@gmail.com>
2022-12-07 09:42:57 +0000

    Day 3 exercise3

--- Section 3 - Day3/day-3-3-exercise.py
+++ Section 3 - Day3/day-3-3-exercise.py
@@ -54,3 +54,20 @@
 #
 # Test Your Code Check your code is doing what it is supposed to. When you're happy with your code, click submit to
 # check your solution.
+
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
