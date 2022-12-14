7e238c6745e6ef09e48e2f63b24aaa165fae25e8
levilu800b <levilu800b@gmail.com>
2022-12-14 08:54:57 +0000

    Day 4 exercise 2

--- Section 4 - Day4/day-4-2-exercise.py
+++ Section 4 - Day4/day-4-2-exercise.py
@@ -27,9 +27,27 @@
 
 # Import the random module here
 
+import random
+
 # Split string method
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
+
+# Get the total number of names in list.
+
+total_names_in_list = len(names)
+
+# Generate random numbers between 0 and the last index.
+
+random_choice = random.randint(0, total_names_in_list - 1)
+
+# Pick out random person from list of names using the random number.
+
+person_who_will_pay = names[random_choice]
+
+# Print the result.
+
+print(f"{person_who_will_pay} is going to buy the meal today!")
