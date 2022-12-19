7cb7d05308d0120dadf4e428dcd29f7eaf2d74ba
levilu800b <levilu800b@gmail.com>
2022-12-19 09:59:11 +0000

    day 5 exercise 4

--- Section 5 - Day5/day-5-4-exercise.py
+++ Section 5 - Day5/day-5-4-exercise.py
@@ -39,3 +39,13 @@
 # Check your code is doing what it is supposed to. When you're happy with your code, click submit to check your solution.
 
 # Write your code below this row 👇
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
---
+++ Section 5 - Day5/day-5-start.py
@@ -0,0 +1,22 @@
+# for loop
+
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#  print(fruit)
+#  print(fruit + " pie")
+
+# Range loop
+# example
+# for number in range(a, b):
+# print(number)
+
+# for number in range(1, 10):
+# print(number)
+
+# for number in range(1, 11, 3):
+#   print(number)
+
+total = 0
+for number in range(1, 101):
+  total += number
+print(total)
