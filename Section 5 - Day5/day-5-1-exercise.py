2ac932f71d09de3c5e0a4759a2a27063188d61c6
levilu800b <levilu800b@gmail.com>
2022-12-19 08:12:06 +0000

    day 5 exercise 1

--- Section 5 - Day5/day-5-1-exercise.py
+++ Section 5 - Day5/day-5-1-exercise.py
@@ -41,8 +41,16 @@ for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
 
+# Write your code below this row 👇
 
-#Write your code below this row 👇
+# Solution
+total_height = 0
+number_of_students = 0
+for height in student_heights:
+    total_height += height
+    number_of_students += 1
+average_height = round(total_height / number_of_students)
+print(average_height)
 
 
 
