cc16032030cc4d156c769584c8c14487fd6be22f
levilu800b <levilu800b@gmail.com>
2023-01-05 18:31:45 +0000

    Defining functions

---
+++ Section 6 - Day6/day-6-start.py
@@ -0,0 +1,19 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+
+# Defining a function
+
+# def my_function():
+# Do this
+# Then do this
+# Finally do this
+
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()
