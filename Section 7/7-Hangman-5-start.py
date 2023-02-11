d5a48d87b45d018ce866a2aebc082ae60513290a
levilu800b <levilu800b@gmail.com>
2023-02-11 09:37:01 +0000

    hangman 5 end

--- Section 7/7-Hangman-5-start.py
+++ Section 7/7-Hangman-5-start.py
@@ -4,6 +4,8 @@ import random
 
 # TODO_1: - Update the word list to use the 'word_list' from hangman_words.py
 # Delete this line: word_list = ["ardvark", "baboon", "camel"]
+from hangman_words import word_list
+
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
@@ -11,9 +13,12 @@ end_of_game = False
 lives = 6
 
 # TODO_3: - Import the logo from hangman_art.py and print it at the start of the game.
+from hangman_art import logo
+
+print(logo)
 
 # Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+# print(f'Pssst, the solution is {chosen_word}.')
 
 # Create blanks
 display = []
@@ -24,17 +29,21 @@ while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     # TODO_4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess in display:
+        print(f"You've already guessed {guess}")
 
     # Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
 
     # Check if user is wrong.
     if guess not in chosen_word:
         # TODO_5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
         lives -= 1
         if lives == 0:
             end_of_game = True
@@ -49,4 +58,6 @@ while not end_of_game:
         print("You win.")
 
     # TODO_2: - Import the stages from hangman_art.py and make this error go away.
+    from hangman_art import stages
+
     print(stages[lives])
