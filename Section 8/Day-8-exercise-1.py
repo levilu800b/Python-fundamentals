c433c6c22d2e44288a8b303f369414655a76f168
levilu800b <levilu800b@gmail.com>
2023-03-03 15:40:32 +0000

    Day 8 exercise 1 complete

--- Section 8/Day-8-exercise-1.py
+++ Section 8/Day-8-exercise-1.py
@@ -29,3 +29,43 @@
 #
 # Test Your Code
 # Check your code is doing what it is supposed to. When you're happy with your code, click submit to check your solution.
+
+#Write your code below this line 👇
+
+def paint_calc(height, width, cover):
+  area = height * width
+  cans = area / cover
+  print(f"You'll need {round(cans)} cans of paint.")
+
+  # or
+  # import math
+  #
+  #   def paint_calc(height, width, cover):
+  #   area = height * width
+  #   cans = area / cover
+  #   print(f"You'll need {math.ceil(cans)} cans of paint.")
+
+  # or
+  # import math
+  #
+  #   def paint_calc(height, width, cover):
+  #   area = height * width
+  #   cans = math.ceil(area / cover)
+  #   print(f"You'll need {cans} cans of paint.")
+  #
+
+
+
+
+
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
