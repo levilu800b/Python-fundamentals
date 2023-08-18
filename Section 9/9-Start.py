3622737ee0720f130c458044e432fd313fbe50b1
levilu800b <levilu800b@gmail.com>
2023-08-18 15:28:01 +0100

    Python Dictionaries lists

--- Section 9/9-Start.py
+++ Section 9/9-Start.py
@@ -41,24 +41,24 @@ travel_log = {
 }
 
 # Nesting Dictionary in a Dictionary
-travel_log = {
-    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-    "Germany": {
-        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-        "total_visits": 5,
-    },
-}
+# travel_log = {
+#     "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+#     "Germany": {
+#         "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+#         "total_visits": 5,
+#     },
+# }
 
 # Nesting Dictionary in a List
-travel_log = [
-    {
-        "country": "France",
-        "cities_visited": ["Paris", "Lille", "Dijon"],
-        "total_visits": 12,
-    },
-    {
-        "country": "Germany",
-        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-        "total_visits": 5,
-    },
-]
+# travel_log = [
+#     {
+#         "country": "France",
+#         "cities_visited": ["Paris", "Lille", "Dijon"],
+#         "total_visits": 12,
+#     },
+#     {
+#         "country": "Germany",
+#         "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+#         "total_visits": 5,
+#     },
+# ]
