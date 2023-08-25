0302632c6da2736410027d44b6275b45d9c2958d
levilu800b <levilu800b@gmail.com>
2023-08-25 14:57:18 +0100

    Section 10 End

--- Section 10/10-start.py
+++ Section 10/10-start.py
@@ -14,3 +14,46 @@ def format_name(f_name, l_name):
 print(
     format_name(input("What is your first name? "), input("What is your last name? "))
 )
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+print(add(2, multiply(5, divide(8, 4))))
+
+
+def outer_function(a, b):
+    def inner_function(c, d):
+        return c + d
+
+    return inner_function(a, b)
+
+
+result = outer_function(5, 10)
+print(result)
+
+
+def my_function(a):
+    if a < 40:
+        return
+        print("Terrible")
+    if a < 80:
+        return "Pass"
+    else:
+        return "Great"
+
+
+print(my_function(25))
