14a2cfa6c7bd0c21de8cccf7c52fee7ae1195497
levilu800b <levilu800b@gmail.com>
2023-09-01 13:44:15 +0100

    Section 13 use a debugger debugging

--- Section 13/13-Start.py
+++ Section 13/13-Start.py
@@ -29,25 +29,27 @@
 # if age > 18:
 #     print(f"You can drive at age {age}.")
 
-# Print is Your Friend
-pages = 0
-word_per_page = 0
-pages = int(input("Number of pages: "))
-word_per_page = int(input("Number of words per page: "))
-total_words = pages * word_per_page
-# Always use print to check your codeand see what is going on in your code at
-# a particular point in time to see if it is working as expected. And for
-# debugging.
-print(f"pages = {pages}")
-print(f"word_per_page = {word_per_page}")
-print(total_words)
+# # Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# # Always use print to check your codeand see what is going on in your code at
+# # a particular point in time to see if it is working as expected. And for
+# # debugging.
+# print(f"pages = {pages}")
+# print(f"word_per_page = {word_per_page}")
+# print(total_words)
 
-# #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
 
-# mutate([1,2,3,5,8,13])
+# Use a Debugger
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
+
+
+mutate([1, 2, 3, 5, 8, 13])
